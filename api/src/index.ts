022ec95101def65b13142598c66e9bfd52dbfdc9
olecsandr009 <sasha2005vit@gmail.com>
2024-05-14 19:11:53 +0300

    chore: transferring the project to typescript

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,52 +1,22 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+import express from "express"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import dotenv from "dotenv"
 
-AppDataSource.initialize().then(async () => {
+dotenv.config()
 
-    // create express app
-    const app = express()
-    app.use(bodyParser.json())
+const PORT = process.env.PORT || 5000
+const app = express()
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
+app.use(cookieParser())
+app.use(bodyParser.json())
 
-    // setup express app here
-    // ...
-
-    // start express server
-    app.listen(3000)
-
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
-
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
-
-}).catch(error => console.log(error))
+const server = app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`)
+})
