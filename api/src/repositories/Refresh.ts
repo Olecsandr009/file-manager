f52d385bae0350a3dd6d97bd052e4b7691295552
olecsandr009 <sasha2005vit@gmail.com>
2024-05-18 00:54:13 +0300

    feat: realize refresh session get and delete functions

--- api/src/repositories/Refresh.ts
+++ api/src/repositories/Refresh.ts
@@ -1,23 +1,33 @@
 import { RefreshEntity } from "entity/refresh.entity"
 import { pool } from "../config/db"
-import { FingerprintConfig, FingerprintResult, FingerprintResultComponent } from "express-fingerprint"
+import { FingerprintResult } from "express-fingerprint"
 
 interface ICreateSession {
     _id: number,
     refresh_token: string,
-    finger_print: any
+    finger_print: string
 }
 
-export async function getRefreshSession(refreshToken: string) {}
+export async function getRefreshSession(refreshToken: string) {
+    const response = await pool.getRepository(RefreshEntity).findOneBy({refresh_token: refreshToken});
+
+    if(!response) {
+        return null
+    }
+
+    return response
+}
 
 export async function createRefreshSession(data: ICreateSession) {
     const session = await pool.getRepository(RefreshEntity).create(data)
     const results = await pool.getRepository(RefreshEntity).save(session)
 }
 
-export async function deleteRefreshSession(refreshToken: string) {}
+export async function deleteRefreshSession(refreshToken: string) {
+    await pool.getRepository(RefreshEntity).delete(refreshToken)
+}
 
-export const RefreshService = {
+export const RefreshRepository = {
     getRefreshSession,
     createRefreshSession,
     deleteRefreshSession
