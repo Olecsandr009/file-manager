32511fac93fa4cb52aa6dc6a2ea5716e6962193f
olecsandr009 <sasha2005vit@gmail.com>
2024-05-18 00:55:08 +0300

    feat: realize token check access functions

--- api/src/service/Token.ts
+++ api/src/service/Token.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 dotenv.config();
@@ -7,6 +7,10 @@ dotenv.config();
 interface IPayload {
     _id: number,
     login: string
+}
+
+interface IRequest extends Request {
+    user: string | jwt.JwtPayload
 }
 
 export async function generateAccessToken(payload: IPayload) {
@@ -21,12 +25,40 @@ export async function generateRefreshToken(payload: IPayload) {
     })
 }
 
-export async function checkAccess(req: Request, res: Response, next: NextFunction) {
-    next()
+export async function checkAccess(req: IRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization
+
+    const token = authHeader?.split(" ")?.[1];
+
+    if( !token ) {
+        const error = new Error("Unauthorized")
+        return next(error)
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+        console.log(error, user)
+
+        if(error) {
+            return next(error)
+        }
+
+        req.user = user;
+        next()
+    })
+}
+
+export async function verifyAccessToken(accessToken: string) {
+    return await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+}
+
+export async function verifyRefreshToken(refreshToken: string) {
+    return await jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
 }
 
 export const TokenService = {
     generateAccessToken,
     generateRefreshToken,
-    checkAccess
+    checkAccess,
+    verifyAccessToken,
+    verifyRefreshToken
 }
